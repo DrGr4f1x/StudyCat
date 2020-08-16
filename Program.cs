2ccbe0f7187c963fca4d820692ea11cd3579bb0f
David Elder <elddm1@gmail.com>
2020-08-16 13:19:09 -0700

    Code cleanup.  Added logic to generate cards from book desc.

--- Program.cs
+++ Program.cs
@@ -33,8 +33,48 @@ namespace StudyCat
     class ListBookOptions
     { }
 
+    [Verb("make", HelpText="Makes a new set of cards from an existing book desc.")]
+    class MakeCardsOptions
+    {
+        [Option('p', "path", HelpText = "Directory in which to put the generated book desc file.")]
+        public DirectoryInfo Path { get; set; }
+    }
+
     class Program
     {
+        static bool PromptForFileOverwrite(string filename)
+        {
+            bool bOverwriteFile = true;
+
+            bool bValidChoice = false;
+            while (!bValidChoice)
+            {
+                Console.Write("The file {0} already exists.  Overwrite it?  [Y]es, [N]o, [Q]uit: ", filename);
+                string val = Console.ReadLine();
+                if (val == "Y" || val == "y")
+                {
+                    bValidChoice = true;
+                }
+                else if (val == "N" || val == "n")
+                {
+                    bValidChoice = true;
+                    bOverwriteFile = false;
+                }
+                else if (val == "Q" || val == "q")
+                {
+                    bValidChoice = true;
+                    bOverwriteFile = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Input {0} is not valid", val);
+                }
+            }
+
+            return bOverwriteFile;
+        }
+
         static Book GenerateFromDesc(BookDesc bookDesc)
         {
             Book book = new Book();
@@ -83,37 +123,13 @@ namespace StudyCat
                 Directory.CreateDirectory(opts.Path.FullName);
             }
 
-            string filename = opts.Path.FullName + "\\" + "book.json";
+            string filename = opts.Path.FullName + "\\book.json";
 
             // See if the book.json file already exists.  Prompt for overwrite.
             bool bCreateFile = true;
             if(File.Exists(filename))
             {
-                bool bValidChoice = false;
-                while (!bValidChoice)
-                {
-                    Console.Write("The file {0} already exists.  Overwrite it?  [Y]es, [N]o, [Q]uit: ", filename);
-                    string val = Console.ReadLine();
-                    if (val == "Y" || val == "y")
-                    {
-                        bValidChoice = true;
-                    }
-                    else if (val == "N" || val == "n")
-                    {
-                        bValidChoice = true;
-                        bCreateFile = false;
-                    }
-                    else if (val == "Q" || val == "q")
-                    {
-                        bValidChoice = true;
-                        bCreateFile = false;
-                        return 0;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Input {0} is not valid", filename);
-                    }
-                }
+                bCreateFile = PromptForFileOverwrite(filename);
             }
 
             if(!bCreateFile)
@@ -121,6 +137,7 @@ namespace StudyCat
                 return 0;
             }
 
+            // Create a stubbed-out book desc that the user can fill in
             BookDesc desc = new BookDesc();
             desc.Title = opts.Title.Length == 0 ? "Book Title" : opts.Title;
             desc.Authors = opts.Authors.Length == 0 ? "Authors" : opts.Authors;
@@ -148,8 +165,7 @@ namespace StudyCat
                 desc.Chapters.Add(chapterDesc);
             }
 
-            
-
+            // Serialize the book desc
             string jsonString;
             var options = new JsonSerializerOptions
             {
@@ -166,77 +182,56 @@ namespace StudyCat
             return 0;
         }
 
+        static int RunMakeAndReturnExitCode(MakeCardsOptions opts)
+        {
+            string bookFilename = opts.Path.FullName + "\\book.json";
+            string cardsFilename = opts.Path.FullName + "\\cards.json";
+
+            // If the book desc file doesn't exist, bail out
+            if (!File.Exists(bookFilename))
+            {
+                Console.WriteLine("Input book desc {0} does not exist.  Exiting.", bookFilename);
+                return 1;
+            }
+
+            // Try to deserialize the book desc
+            string jsonString = File.ReadAllText(bookFilename);
+            BookDesc bookDesc = JsonSerializer.Deserialize<BookDesc>(jsonString);
+
+            // Construct a cardset from the book desc
+            Book book = GenerateFromDesc(bookDesc);
+
+            // See if the cards.json file already exists.  Prompt for overwrite.
+            bool bCreateFile = true;
+            if (File.Exists(cardsFilename))
+            {
+                bCreateFile = PromptForFileOverwrite(cardsFilename);
+            }
+
+            if (!bCreateFile)
+            {
+                return 0;
+            }
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            jsonString = JsonSerializer.Serialize(book, options);
+            File.WriteAllText(cardsFilename, jsonString);
+
+            return 0;
+        }
+
         static int Main(string[] args)
         {
-            return CommandLine.Parser.Default.ParseArguments<NewBookOptions, ListBookOptions>(args)
+            return CommandLine.Parser.Default.ParseArguments<NewBookOptions, ListBookOptions, MakeCardsOptions>(args)
                 .MapResult(
                     (NewBookOptions opts) => RunNewAndReturnExitCode(opts),
                     (ListBookOptions opts) => RunListAndReturnExitCode(opts),
+                    (MakeCardsOptions opts) => RunMakeAndReturnExitCode(opts),
                     errs => 1
                 );
-
-            //Console.WriteLine("Hello World!");
-
-            //BookDesc bookDesc = new BookDesc();
-            //bookDesc.Title = "Calculus";
-            //bookDesc.Authors = "Rogawski, Jon; Adams, Colin";
-            //bookDesc.Year = "2015";
-            //bookDesc.Publisher = "W. H. Freeman and Company";
-
-            //ChapterDesc chap2 = new ChapterDesc();
-            //chap2.Title = "Limits";
-            //chap2.Number = 2;
-
-            //SectionDesc sec2_1 = new SectionDesc();
-            //sec2_1.Title = "Limits, Rates of Change, and Tangent Lines";
-            //sec2_1.Number = 1;
-            //sec2_1.Pages = "pp. 44-47";
-            //sec2_1.NumProblems = 36;
-
-            //SectionDesc sec2_2 = new SectionDesc();
-            //sec2_2.Title = "Limits: A Numerical and Graphical Approach";
-            //sec2_2.Number = 2;
-            //sec2_2.Pages = "pp. 54-56";
-            //sec2_2.NumProblems = 70;
-
-            //chap2.Sections.Add(sec2_1);
-            //chap2.Sections.Add(sec2_2);
-
-            //ChapterDesc chap3 = new ChapterDesc();
-            //chap3.Title = "Differentiation";
-            //chap3.Number = 3;
-
-            //SectionDesc sec3_1 = new SectionDesc();
-            //sec3_1.Title = "Definition of the Derivative";
-            //sec3_1.Number = 1;
-            //sec3_1.Pages = "pp. 102-105";
-            //sec3_1.NumProblems = 73;
-
-            //SectionDesc sec3_2 = new SectionDesc();
-            //sec3_2.Title = "The Derivative as a Function";
-            //sec3_2.Number = 2;
-            //sec3_2.Pages = "pp. 114-117";
-            //sec3_2.NumProblems = 98;
-
-            //chap3.Sections.Add(sec3_1);
-            //chap3.Sections.Add(sec3_2);
-
-            //bookDesc.Chapters.Add(chap2);
-            //bookDesc.Chapters.Add(chap3);
-
-            //string jsonString;
-            //var options = new JsonSerializerOptions
-            //{
-            //    WriteIndented = true
-            //};
-            //jsonString = JsonSerializer.Serialize(bookDesc, options);
-            //string filename = "book.json";
-            //File.WriteAllText(filename, jsonString);
-
-            //Book book = GenerateFromDesc(bookDesc);
-            //jsonString = JsonSerializer.Serialize(book, options);
-            //filename = "problems.json";
-            //File.WriteAllText(filename, jsonString);
         }
     }
 }
